861edc33b25293669f4400ff96bcc06611adf172
TaeHo Yun <ice_98@naver.com>
2024-04-23 17:42:41 +0900

    Feat: station entity 생성

--- backend/bandi/src/main/java/org/banditbul/bandi/station/entity/Station.java
+++ backend/bandi/src/main/java/org/banditbul/bandi/station/entity/Station.java
@@ -1,5 +1,24 @@
 package org.banditbul.bandi.station.entity;
 
-public class Station {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity
+public class Station {
+    @Id
+    @Column(name = "station_id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "login_id")
+    private String loginID;
+
+    @Column(name = "login_pw")
+    private String password;
+
+    @Column(name = "line")
+    private int line;
 }
