507f66fd09c176a0e5083b141b65c3a7e7c8f739
TaeHo Yun <ice_98@naver.com>
2024-04-25 17:49:30 +0900

    Fix: 테스트db 주석처리

--- backend/bandi/src/test/java/org/banditbul/bandi/BandiApplicationTests.java
+++ backend/bandi/src/test/java/org/banditbul/bandi/BandiApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
-class BandiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//class BandiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
