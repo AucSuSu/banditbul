1a0db41f0377d6d31e3772fa85eea802923fc449
TaeHo Yun <ice_98@naver.com>
2024-04-26 17:44:41 +0900

    Feat: CORS 설정
    Change-Id: Icc02d5bdbbf17b39c5c24f1f0797c6d3ee205074

--- backend/bandi/src/main/java/org/banditbul/bandi/config/CorsConfig.java
+++ backend/bandi/src/main/java/org/banditbul/bandi/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package org.banditbul.bandi.config;
 
-import
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
