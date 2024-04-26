4255de29fc53531b5dc5ebbf4d7e7bd5861c605e
TaeHo Yun <ice_98@naver.com>
2024-04-26 17:21:05 +0900

    Feat: 세션 로그인 완료
    Change-Id: If726c5210445b52e92d228b1feeea73121305ec9

--- backend/bandi/src/main/java/org/banditbul/bandi/screendoor/controller/ScreendoorController.java
+++ backend/bandi/src/main/java/org/banditbul/bandi/screendoor/controller/ScreendoorController.java
@@ -5,12 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.banditbul.bandi.common.HttpStatusEnum;
 import org.banditbul.bandi.common.Message;
 import org.banditbul.bandi.screendoor.Service.ScreendoorService;
+import org.springframework.http.HttpCookie;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -20,7 +18,8 @@ public class ScreendoorController {
 
     private final ScreendoorService screendoorService;
     @GetMapping("/stationinfo/{beaconId}")
-    public ResponseEntity<Message> getStationName(@PathVariable(value = "beaconId") String beaconId){
+    public ResponseEntity<Message> getStationName(@PathVariable(value = "beaconId") String beaconId, @CookieValue(name = "SESSION", defaultValue = "defaultUser") String username){
+        System.out.println(username);
 
         String stationName = screendoorService.getStationName(beaconId);
         Message message = new Message(HttpStatusEnum.OK, stationName + " 역 이름 출력 완료", stationName);
