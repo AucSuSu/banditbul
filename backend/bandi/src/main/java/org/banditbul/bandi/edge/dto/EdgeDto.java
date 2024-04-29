466d378e9e0189aee47a7ed3d8cfe1997efcf4ab
TaeHo Yun <ice_98@naver.com>
2024-04-29 17:33:38 +0900

    Feat: addEdge api 완료
    Change-Id: Ief597ab427f8a87a59e18e51bfeb65879f383a96

---
+++ backend/bandi/src/main/java/org/banditbul/bandi/edge/dto/EdgeDto.java
@@ -0,0 +1,13 @@
+package org.banditbul.bandi.edge.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EdgeDto {
+    private int point1;
+    private int point2;
+    private int stationId;
+    private int distance;
+}
