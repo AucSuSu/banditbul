1d1227e529029cb68ea082a613ef62f4d7528074
woomigyeong <migyom222222@gmail.com>
2024-04-29 14:36:28 +0900

    Feat: 길찾기 API 개발 중
    Change-Id: I01251c661cba2f4a36687219d2a3091f2f9596ad

--- backend/bandi/src/main/java/org/banditbul/bandi/point/repository/PointRepository.java
+++ backend/bandi/src/main/java/org/banditbul/bandi/point/repository/PointRepository.java
@@ -1,7 +1,13 @@
 package org.banditbul.bandi.point.repository;
 
+import org.banditbul.bandi.gate.entity.Gate;
 import org.banditbul.bandi.point.entity.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PointRepository extends JpaRepository<Point, Integer> {
+
+    public Optional<List<Point>> findAllByStationId(int stationId);
 }
--- backend/bandi/src/main/java/org/banditbul/bandi/station/repository/StationRepository.java
+++ backend/bandi/src/main/java/org/banditbul/bandi/station/repository/StationRepository.java
@@ -3,6 +3,10 @@ package org.banditbul.bandi.station.repository;
 import org.banditbul.bandi.station.entity.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StationRepository extends JpaRepository<Station, Integer> {
     public Station findByLoginId(String loginId);
+
+    public Optional<Station> findByName(String stationName);
 }
