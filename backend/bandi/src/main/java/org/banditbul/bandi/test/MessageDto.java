d6ba8206f781e5210c5ebed70c51dc57b5d13cc5
Rael <allice2283006@gmail.com>
2024-05-02 10:50:52 +0900

    Fix: 세션 타입 멀티스레드로 변경
    Change-Id: I6667c01abe19951b3464e7dc0927bcadba78b9ff

--- backend/bandi/src/main/java/org/banditbul/bandi/test/MessageDto.java
+++ backend/bandi/src/main/java/org/banditbul/bandi/test/MessageDto.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class MessageDto {
-    private String type;
-    private String content;
-    private String sessionId;
+    private String type; //메세지 유형
+    private String content; //메세지 내용
+    private String sessionId; //속한 세션
 }
