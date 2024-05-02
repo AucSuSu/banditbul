17346aa614799507eae0e9aa87d0c36bfc21553f
Rael <allice2283006@gmail.com>
2024-05-02 14:41:37 +0900

    Fix: 비콘 별 사용자 수 전달하도록 수정
    Change-Id: I86fb563ce3ef8c49d262515ed959aa416b6a0d79

--- backend/bandi/src/main/java/org/banditbul/bandi/test/MessageDto.java
+++ backend/bandi/src/main/java/org/banditbul/bandi/test/MessageDto.java
@@ -2,8 +2,13 @@ package org.banditbul.bandi.test;
 
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 public class MessageDto {
     private Type type; //메세지 유형
+    private Map<String, Integer> count; //비콘 수
     private String beaconId; //비콘 id
+    private String sessionId; //세션 id
+    private String uuId; //유저 id
 }
