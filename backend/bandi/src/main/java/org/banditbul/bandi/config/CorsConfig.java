afab06e5591cb06bbb08f5f979ebe2263e9032d2
TaeHo Yun <ice_98@naver.com>
2024-05-07 11:22:11 +0900

    Fix: cors ip 추가
    Change-Id: Iea08b345d0b7967535743ee27d54576aaa0ccee7

--- backend/bandi/src/main/java/org/banditbul/bandi/config/CorsConfig.java
+++ backend/bandi/src/main/java/org/banditbul/bandi/config/CorsConfig.java
@@ -16,6 +16,8 @@ public class CorsConfig {
         config.setAllowCredentials(true); //내 서버가 응답을 할 때 json을 자바스크립트에서 처리할 수 있게 할지를 설정하는 것
         config.addAllowedOrigin("http://192.168.30.152:5173");
         config.addAllowedOrigin("http://192.168.30.107:5173");
+        config.addAllowedOrigin("192.168.30.152:5173");
+        config.addAllowedOrigin("192.168.30.107:5173");
         config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedOrigin("https://banditbul.co.kr");
         config.addAllowedHeader("*"); // 모든 header에 응답을 허용하겠다.
