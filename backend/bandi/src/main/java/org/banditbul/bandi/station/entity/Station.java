5ee4e151614177615151f1db65e1921b98cb011a
TaeHo Yun <ice_98@naver.com>
2024-05-07 12:14:35 +0900

    Fix: station에 ToString을 추가하면 LazyInitialization 에러가 나오는거 해결
    Change-Id: Ie349c886f935fe47bdd0dd1e6d7e038660e7b167

--- backend/bandi/src/main/java/org/banditbul/bandi/station/entity/Station.java
+++ backend/bandi/src/main/java/org/banditbul/bandi/station/entity/Station.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@ToString
+//@ToString
 public class Station {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
