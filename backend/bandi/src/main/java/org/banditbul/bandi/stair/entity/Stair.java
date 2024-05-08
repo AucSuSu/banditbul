20c578c5e837875c42ab9587cf42693710376400
woomigyeong <migyom222222@gmail.com>
2024-05-08 12:20:45 +0900

    Fix: 엔티티에 cascade 설정
    Change-Id: I66d4a366772bc52bd0a2707948be83d2c8899f40

--- backend/bandi/src/main/java/org/banditbul/bandi/stair/entity/Stair.java
+++ backend/bandi/src/main/java/org/banditbul/bandi/stair/entity/Stair.java
@@ -17,7 +17,7 @@ public class Stair {
     @Column(name = "stair_id")
     private Integer id;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
     @JoinColumn(name = "beacon_id")
     private Beacon beacon;
 
