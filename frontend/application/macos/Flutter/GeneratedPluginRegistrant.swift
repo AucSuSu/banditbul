6d09122159162d0eb6d1d180789558a0415c2382
강문수 <dalsu1742@gmail.com>
2024-05-08 10:04:11 +0900

    Feat: beacon
    Change-Id: I20947b10ee1821146a6738f3dba23ed9ea3d9236

--- frontend/application/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/application/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,12 +6,14 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import flutter_blue_plus
 import flutter_tts
 import location
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
