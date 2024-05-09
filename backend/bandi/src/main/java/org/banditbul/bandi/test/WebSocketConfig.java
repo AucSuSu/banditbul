0504b042fcea007583d7d3681d31e623cc6cfcdf
TaeHo Yun <ice_98@naver.com>
2024-05-10 02:46:00 +0900

    Fix: 웹소켓 sos신호 완료
    Change-Id: I3c8520249e6b3f56a4441ec7bf116ce3c074bc2c

--- backend/bandi/src/main/java/org/banditbul/bandi/test/WebSocketConfig.java
+++ backend/bandi/src/main/java/org/banditbul/bandi/test/WebSocketConfig.java
@@ -1,27 +1,21 @@
 package org.banditbul.bandi.test;
 
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+@Slf4j
+@Configuration
 @RequiredArgsConstructor
 @EnableWebSocket
-@Configuration
-public class WebsocketConfig implements WebSocketConfigurer {
-    private final WebSocketHandler webSocketHandler;
-
-    // Create a logger instance for this class
-    private static final Logger log = LoggerFactory.getLogger(WebsocketConfig.class);
-
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final WebSocketHandler1 webSocketHandler;
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         log.debug("Registering WebSocket handlers");
         registry.addHandler(webSocketHandler, "/socket").setAllowedOrigins("*");
-        log.info("WebSocket handler registered for '/socket' with allowed origins '*'");
     }
 }
